bd4e88f315bfabc182d71209b9be719e889c4fec
Akarsh Shrivastava <82137709+AkarshShrivastava2@users.noreply.github.com>
2021-06-02 00:10:09 +0530

    Update tracker.jsx

--- src/component/tracker.jsx
+++ src/component/tracker.jsx
@@ -5,7 +5,7 @@ import Subtracker from './subtracker';
 const Tracker = () => {
   return (
     <>
-      <div className=" col-lg-10 col-md-9 col-sm-10 container mx-auto text-center">
+      <div className=" col-lg-10 col-md-9 col-sm-12 container mx-auto my-4 text-center">
         <h1> INDIA Covid-19 Dashboard </h1>
         <Subtracker />
       </div>
