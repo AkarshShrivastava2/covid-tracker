79ac8e5ea57914baf642a21703a9026cdfdd55f4
Akarsh Shrivastava <82137709+AkarshShrivastava2@users.noreply.github.com>
2021-06-01 23:34:04 +0530

    Update tracker.jsx

--- src/component/tracker.jsx
+++ src/component/tracker.jsx
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
-import Button from 'react-bootstrap/Button';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card' ;
 import Subtracker from './subtracker';
 
 
